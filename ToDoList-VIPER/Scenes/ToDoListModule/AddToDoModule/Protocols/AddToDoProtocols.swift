b46dbc593a7a2504eaff71938952021798d17ffd
BorisKiselev <hugh.dancy@icloud.com>
2023-11-15 15:54:54 +0700

    Add AddToDoPorotocols.

--- ToDoList-VIPER/Scenes/ToDoListModule/AddToDoModule/Protocols/AddToDoProtocols.swift
+++ ToDoList-VIPER/Scenes/ToDoListModule/AddToDoModule/Protocols/AddToDoProtocols.swift
@@ -8,5 +8,32 @@
 import UIKit
 
 protocol AddToDoViewProtocol: AnyObject {
+    var presenter: AddToDoPresenterProtocol? { get set }
+}
+
+protocol AddToDoPresenterProtocol: AnyObject {
+    var view: AddToDoViewProtocol? { get set }
+    var interactor: AddToDoInteractorInputProtocol? { get set }
+    var router: AddToDoRouterProtocol? { get set }
+    
+    //VIEW -> PRESENTER
+    func addToDo(_ toDoItem: ToDoItem)
+
+}
+
+protocol AddToDoInteractorInputProtocol: AnyObject {
+    var presenter: AddToDoInteractorOutputProtocol? { get set }
+    
+    //PRESENTER -> INTERACTOR
+    func saveToDo(_ toDoItem: ToDoItem)
+}
+
+protocol AddToDoInteractorOutputProtocol: AnyObject {
+    //INTERACTOR -> PRESENTER
+    func didAddToDo(_ toDoItem: ToDoItem)
+}
+
+protocol AddToDoRouterProtocol: AnyObject {
+    static func createAddToDoModule() -> UIViewController
     
 }
