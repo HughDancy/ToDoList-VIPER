23afe623910613b3fc69c422225aaf35cf92611a
BorisKiselev <hugh.dancy@icloud.com>
2024-01-12 15:57:10 +0700

    Setup LaunchScreen, create AnimationLoadingController.
    Setup LaunchScreen, add new folders, create ALVc.

---
+++ ToDoList-VIPER/Scenes/Loading/AnimationLoadingController.swift
@@ -0,0 +1,29 @@
+//
+//  AnimationLoadingController.swift
+//  ToDoList-VIPER
+//
+//  Created by Борис Киселев on 12.01.2024.
+//
+
+import UIKit
+
+class AnimationLoadingController: UIImagePickerController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
