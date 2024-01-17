931fc7e19ec6e92d7ffe109adac6bef8b50c2c9b
BorisKiselev <hugh.dancy@icloud.com>
2024-01-17 15:37:01 +0700

    Create folders in Onboarding Module.

---
+++ ToDoList-VIPER/Scenes/Onboarding/View/WelcomeController.swift
@@ -0,0 +1,21 @@
+//
+//  WelcomeController.swift
+//  ToDoList-VIPER
+//
+//  Created by Борис Киселев on 17.01.2024.
+//
+
+import UIKit
+
+class WelcomeController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+ 
+
+}
