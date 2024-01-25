a898da13aa883b4046d8814f8b189ab9bced67cd
BorisKiselev <hugh.dancy@icloud.com>
2024-01-25 15:10:11 +0700

    Create LoginProtocols.

--- ToDoList-VIPER/Scenes/Login/Protocols/LoginProtocols.swift
+++ ToDoList-VIPER/Scenes/Login/Protocols/LoginProtocols.swift
@@ -13,5 +13,27 @@ protocol LoginViewProtocol: AnyObject {
 }
 
 protocol LoginPresenterProtocol: AnyObject {
+    var view: LoginViewProtocol? { get set }
+    var interactor: LoginInteractorInputProtocol? { get set }
+    var router: LoginRouterProtocol? { get set }
     
+    //VIEW -> PRESENTER
+    func chekTheLogin(login: String, password: String)
+}
+
+protocol LoginInteractorInputProtocol: AnyObject {
+    var presenter: LoginInteractorOutputProtocol? { get set }
+    
+    //PRESENTER -> INTERACTOR
+    func checkAutorizationData(login: String, password: String)
+}
+
+protocol LoginInteractorOutputProtocol: AnyObject {
+    //INTERACTOR -> PRESENTER
+    func getVerificationResult() -> Bool
+}
+
+protocol LoginRouterProtocol: AnyObject {
+    static func createLoginModule() -> UIViewController
+    func goToMainScreen(from view: LoginViewProtocol, bool: Bool)
 }
