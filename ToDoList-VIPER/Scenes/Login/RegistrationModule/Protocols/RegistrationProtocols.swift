2422d9c6fbd09a134706528b450f8e75f14a3088
BorisKiselev <hugh.dancy@icloud.com>
2024-01-29 13:43:29 +0700

    Create first version of RegisterProtocols.

---
+++ ToDoList-VIPER/Scenes/Login/RegistrationModule/Protocols/RegistrationProtocols.swift
@@ -0,0 +1,39 @@
+//
+//  RegistrationProtocols.swift
+//  ToDoList-VIPER
+//
+//  Created by Борис Киселев on 29.01.2024.
+//
+
+import UIKit
+
+protocol RegistrationViewProtocol: AnyObject {
+    var presenter: RegistrationPresenterPtorocol? { get set }
+}
+
+protocol RegistrationPresenterPtorocol: AnyObject {
+    var view: RegistrationViewProtocol? { get set }
+    var interactor: RegistrationInteractorInputProtocol? { get set }
+    var rotuter: RegistrationRouterProtocol? { get set }
+    
+    //VIEW -> PRESENTER
+    func registerNewUser(with name: String, email: String, password: String)
+}
+
+protocol RegistrationInteractorInputProtocol: AnyObject {
+    var presenter: RegistrationInteractorOutputProtocol? { get set }
+    
+    //PRESENTER -> INTERACTOR
+    func registerNewUser(name: String, email: String, password: String)
+}
+
+protocol RegistrationInteractorOutputProtocol: AnyObject {
+    //INTERACTOR -> PRESENTER
+    func getRegistrationResult() -> Bool
+}
+
+protocol RegistrationRouterProtocol: AnyObject {
+    static func createRegistrationModule() -> UIViewController
+    func showAlert()
+    func dismissRegister()
+}
