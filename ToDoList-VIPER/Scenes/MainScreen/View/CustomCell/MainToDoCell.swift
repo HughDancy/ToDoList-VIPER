2c32168f64ee319d1559414fcd6ff107a905ca7c
BorisKiselev <hugh.dancy@icloud.com>
2024-03-14 17:15:50 +0700

    Make MainToDoCell for CollectioView.

--- ToDoList-VIPER/Scenes/MainScreen/View/CustomCell/MainToDoCell.swift
+++ ToDoList-VIPER/Scenes/MainScreen/View/CustomCell/MainToDoCell.swift
@@ -8,14 +8,84 @@
 import UIKit
 
 class MainToDoCell: UICollectionViewCell {
+    static let reuseIdentifier = "MainToDoCell"
     
+    //MARK: - Outlets
+    private lazy var containerView: UIView = {
+        let container = UIView()
+        container.backgroundColor = .systemGray4
+        container.layer.cornerRadius = 15
+        container.clipsToBounds = true
+        return container
+    }()
+    
+    private lazy var arrowIcon: UIImageView = {
+        let arrow = UIImageView()
+        arrow.image = UIImage(systemName: "arrow.right.circle.fill")
+        arrow.tintColor = .systemGreen
+        return arrow
+    }()
+    
+    private lazy var numbersLabel: UILabel = {
+        let numbers = UILabel()
+        
+        return numbers
+    }()
+    
+    private lazy var toDosLabel: UILabel = {
+        let label = UILabel()
+        
+        return label
+    }()
     
     //MARK: - Init
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setupView()
     }
     
     required init?(coder: NSCoder) {
-        super.init(coder: coder)
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupView() {
+        self.backgroundColor = .clear
+        setupHierarchy()
+        setupLayout()
+    }
+    
+    //MARK: - Setup Hierarchy
+    private func setupHierarchy() {
+        containerView.addSubview(containerView)
+        containerView.addSubview(arrowIcon)
+        containerView.addSubview(numbersLabel)
+        containerView.addSubview(toDosLabel)
+    }
+    
+    //MARK: - Setup Layout
+    private func setupLayout() {
+        containerView.snp.makeConstraints { make in
+            make.top.leading.trailing.bottom.equalToSuperview().inset(10)
+        }
+        
+        arrowIcon.snp.makeConstraints { make in
+            make.top.trailing.equalToSuperview().inset(15)
+        }
+        
+        numbersLabel.snp.makeConstraints { make in
+            make.top.equalTo(arrowIcon.snp.bottom).offset(15)
+            make.centerX.equalToSuperview()
+        }
+        
+        toDosLabel.snp.makeConstraints { make in
+            make.top.equalTo(numbersLabel.snp.bottom).offset(10)
+            make.centerX.equalToSuperview()
+        }
+    }
+    
+    //MARK: - Output setup outlets method
+    func setupElements(numbers: Int, dayLabel: String) {
+        numbersLabel.text = String(numbers)
+        toDosLabel.text = dayLabel
     }
 }
