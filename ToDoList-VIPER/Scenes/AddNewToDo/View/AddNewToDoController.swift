e726091deb9e26c91faa56a34c1b2da179b1839c
BorisKiselev <hugh.dancy@icloud.com>
2024-03-15 23:03:32 +0700

    Start implement AddNewToDoController.

--- ToDoList-VIPER/Scenes/AddNewToDo/View/AddNewToDoController.swift
+++ ToDoList-VIPER/Scenes/AddNewToDo/View/AddNewToDoController.swift
@@ -8,10 +8,38 @@
 import UIKit
 
 class AddNewToDoController: UIViewController {
-
+    
     //MARK: - OUTLETS
-    private lazy var nameOfTaskField = UITextField.createBasicTextField(textSize: 15, weight: .semibold, borderStyle: .roundedRect, returnKey: .next, tag: 0)
-    private lazy var descriptionField = UITextField.createBasicTextField(textSize: 15, weight: .semibold, borderStyle: .roundedRect, returnKey: .done, tag: 1)
+    private lazy var titleOfScreen: UILabel = {
+        let label = UILabel()
+        label.text = "Добавить задачу"
+        label.font = UIFont.systemFont(ofSize: 35, weight: .bold)
+        label.textColor = .systemCyan
+        return label
+    }()
+    
+    private lazy var nameOfTaskField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .none
+        textField.backgroundColor = .systemGray5
+        textField.font = UIFont.systemFont(ofSize: 15, weight: .semibold)
+        textField.placeholder = "Наименование задачи"
+        textField.layer.cornerRadius = 10
+        textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        textField.leftViewMode = .always
+        return textField
+    }()
+    
+    private lazy var descriptionField: UITextView = {
+       let textView = UITextView()
+//        textView.font = UIFont.systemFont(ofSize: 15, weight: .semibold)
+//        textView.placeholderText = "Описание задачи"
+        textView.backgroundColor = .systemGray5
+        textView.layer.cornerRadius = 10
+        
+        return textView
+    }()
+ 
     private lazy var dateField = UIDatePicker.createToDoPicker()
     private lazy var addNewToDoButton = UIButton.createToDoButton(title: "Add new ToDo", backColor: .systemCyan, tintColor: .systemBackground)
     
@@ -26,33 +54,39 @@ class AddNewToDoController: UIViewController {
     
     //MARK: - Setup Hierarchy
     private func setupHierarchy() {
+        view.addSubview(titleOfScreen)
         view.addSubview(nameOfTaskField)
         view.addSubview(descriptionField)
-        view.addSubview(dateField)
+//        view.addSubview(dateField)
         view.addSubview(addNewToDoButton)
     }
     //MARK: - Setup Layout
     private func setupLayout() {
-        nameOfTaskField.snp.makeConstraints { make in
+        titleOfScreen.snp.makeConstraints { make in
             make.top.equalToSuperview().offset(50)
+            make.centerX.equalToSuperview()
+        }
+        
+        nameOfTaskField.snp.makeConstraints { make in
+            make.top.equalTo(titleOfScreen.snp.bottom).offset(50)
             make.leading.trailing.equalToSuperview().inset(40)
-            make.height.equalTo(30)
+            make.height.equalTo(50)
         }
         
         descriptionField.snp.makeConstraints { make in
             make.top.equalTo(nameOfTaskField.snp.bottom).offset(10)
             make.leading.trailing.equalToSuperview().inset(40)
-            make.height.equalTo(30)
+            make.height.equalTo(100)
         }
         
-        dateField.snp.makeConstraints { make in
-            make.top.equalTo(descriptionField.snp.bottom).offset(10)
-            make.leading.trailing.equalToSuperview().inset(40)
-        }
+//        dateField.snp.makeConstraints { make in
+//            make.top.equalTo(descriptionField.snp.bottom).offset(10)
+//            make.leading.trailing.equalToSuperview().inset(40)
+//        }
         
         addNewToDoButton.snp.makeConstraints { make in
-            make.top.equalTo(dateField.snp.bottom).offset(30)
-            make.leading.trailing.equalTo(50)
+            make.bottom.equalToSuperview().inset(50)
+            make.leading.trailing.equalToSuperview().inset(50)
             make.height.equalTo(50)
             
         }
@@ -64,7 +98,10 @@ class AddNewToDoController: UIViewController {
     @objc func addNewToDo() {
         
     }
-
- 
-
+    
+    @objc func removeTextInTextView() {
+        self.descriptionField.text = ""
+    }
+    
+    
 }
