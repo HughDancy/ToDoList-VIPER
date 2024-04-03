8a15e47f4034718061ad29558ccccc4cc71fdda7
BorisKiselev <hugh.dancy@icloud.com>
2024-04-03 16:43:27 +0700

    Create Protocols for Module.

---
+++ ToDoList-VIPER/Scenes/ToDosDetail/Protocols/ToDosDetailProtocols.swift
@@ -0,0 +1,47 @@
+//
+//  ToDosDetailProtocols.swift
+//  ToDoList-VIPER
+//
+//  Created by Борис Киселев on 03.04.2024.
+//
+
+import UIKit
+
+protocol ToDosDetailViewProtocol: AnyObject {
+    var presenter: ToDosDetailPresenterProtocol? { get set }
+    
+    func showToDoItem(_ toDo: ToDoObject)
+}
+
+protocol ToDosDetailPresenterProtocol: AnyObject {
+    var view: ToDosDetailViewProtocol? { get set }
+    var interactor: ToDosDetailInteractorInputProtocol? { get set }
+    var router: ToDosDetailRouterProtocol? { get set }
+    
+    //VIEW -> PRESENTER
+    func viewWillAppear()
+    func editToDo(title: String, descriprion: String, date: String, date: Date, color: String)
+    func deleteToDo(_ toDo: ToDoObject)
+}
+
+protocol ToDosDetailInteractorInputProtocol: AnyObject {
+    var presenter: ToDosDetailInteractorOutputProtocol? { get set }
+    var toDoItem: ToDoObject? { get set }
+    
+    //PRESENTER -> INTERACTOR
+    func editTask(title: String, descriprion: String, date: String, date: Date, color: String)
+    func deleteTask(_ toDo: ToDoObject)
+}
+
+protocol ToDosDetailInteractorOutputProtocol: AnyObject {
+    //INTERACTOR -> PRESTNER
+    func didDeleteToDo()
+    func didEditToDo(_ toDo: ToDoObject)
+}
+
+protocol ToDosDetailRouterProtocol: AnyObject {
+    static func createModule(with toDo: ToDoObject) -> UIViewController
+    
+    //PRESENTER -> ROUTER
+    func goBackToTasks(with view: ToDosDetailViewProtocol)
+}
