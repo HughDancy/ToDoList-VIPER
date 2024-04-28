6238177e6429ce7f4842eb31e7003eb57638e5d8
BorisKiselev <hugh.dancy@icloud.com>
2024-04-28 13:33:21 +0700

    Add UIColor Extension.

--- ToDoList-VIPER/Support/Extension/UIColor + EXT.swift
+++ ToDoList-VIPER/Support/Extension/UIColor + EXT.swift
@@ -8,6 +8,16 @@
 import UIKit
 
 extension UIColor {
+    var coreImageColor: CIColor {
+        return CIColor(color: self)
+    }
+    var components: (red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) {
+        let coreImageColor = self.coreImageColor
+        return (coreImageColor.red, coreImageColor.green, coreImageColor.blue, coreImageColor.alpha)
+    }
+}
+
+extension UIColor {
     static func convertStringToColor(_ string: String) -> UIColor {
         switch string {
         case "systemOrange":
@@ -32,5 +42,33 @@ extension UIColor {
         default:
             return "systemBlue"
         }
+    }
+    
+    static func getColorFromDict(dict: [String : Any]) -> UIColor? {
+        guard let red = dict["red"] as? NSNumber,
+              let green = dict["green"] as? NSNumber,
+              let blue = dict["blue"] as? NSNumber,
+              let alpha = dict["alpha"] as? NSNumber else {
+            return nil
+        }
+
+        return UIColor(red: CGFloat(truncating: red) ,
+                       green: CGFloat(truncating: green) ,
+                       blue: CGFloat(truncating: blue) ,
+                       alpha: CGFloat(truncating: alpha))
+    }
+    
+    static func getDictFromColor(color: UIColor) -> [String : Any] {
+        let color = color.coreImageColor
+        let redValue = color.red
+        let greenValue = color.green
+        let blueValue = color.blue
+        let alphaValue = color.alpha
+        
+        return ["red" : redValue,
+                "green" : greenValue,
+                "blue" : blueValue,
+                "alpha" : alphaValue
+        ]
     }
 }
