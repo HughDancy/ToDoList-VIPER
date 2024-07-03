3e9897eabfcd1e56b825b89c7728f16d32dba232
BorisKiselev <hugh.dancy@icloud.com>
2024-07-03 17:06:07 +0700

    Add Options Protocols for module.

--- ToDoList-VIPER/Scenes/Options/Protocols/OptionsProtocols.swift
+++ ToDoList-VIPER/Scenes/Options/Protocols/OptionsProtocols.swift
@@ -6,3 +6,40 @@
 //
 
 import UIKit
+
+protocol OptionsViewProtocol: AnyObject {
+    var presenter: OptionsPresenterProtocol? { get set }
+    
+    func getOptionsData(_ data: [String])
+    func getUserData(_ userData: (String, URL))
+}
+
+protocol OptionsPresenterProtocol: AnyObject {
+    var view: OptionsPresenterProtocol? { get set }
+    var interactor: OptionsInputInteractorProtocol? { get set }
+    var router: OptionsRouterProtocol? { get set }
+    
+    func getData()
+    func goToUserOptions(from view: OptionsViewProtocol)
+    func logOut(from view: OptionsViewProtocol)
+    func changeTheme()
+}
+
+protocol OptionsInputInteractorProtocol: AnyObject {
+    var presenter: OptionsOutputInteractorProtocol? { get set }
+    func fetchOptionsData()
+    func getUserData()
+    func changeTheme()
+}
+
+
+protocol OptionsOutputInteractorProtocol: AnyObject {
+    func getOptionsData(_ data: [String])
+    func getUserData(_ userData: (String, URL))
+}
+
+protocol OptionsRouterProtocol: AnyObject {
+    func createOptionsModule() -> UIViewController
+    func goToUserOptions(from view: OptionsViewProtocol)
+    func logOut(from view: OptionsViewProtocol)
+}
